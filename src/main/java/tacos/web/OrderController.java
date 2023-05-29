700297a918c8c0a1d7b3cabbd344a89898afe411
byodian <byj6696@gmail.com>
2023-05-29 23:28:39 +0800

    displaying validation errors

--- src/main/java/tacos/web/OrderController.java
+++ src/main/java/tacos/web/OrderController.java
@@ -1,7 +1,9 @@
 package tacos.web;
 
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,8 +22,14 @@ public class OrderController {
     }
 
     @PostMapping
-    public String processOrder(TacoOrder order, SessionStatus sessionStatus) {
+    public String processOrder(@Valid TacoOrder order, Errors errors, SessionStatus sessionStatus) {
+        if (errors.hasErrors()) {
+            log.error("Validation errors: {}", errors.getAllErrors());
+            return "orderForm";
+        }
+
         log.info("Processing order: " + order);
+
         // clean up the session
         sessionStatus.setComplete();
         return "redirect:/";
